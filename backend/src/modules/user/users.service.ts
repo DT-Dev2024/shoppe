2272bdfaef4dc6b514d971adb18fe7ca7b8c3676
VNKaiser <tandatvok16@gmail.com>
2024-06-29 09:48:05 +0700

    fix: api address

--- backend/src/modules/user/users.service.ts
+++ backend/src/modules/user/users.service.ts
@@ -47,8 +47,20 @@ export class UsersService {
   }
 
   async updateAddress(address: CreateAddressDto) {
+    // If the address has an ID, it's an update operation
     if (address.id !== '') {
-      const update = await this.prismaService.addresses.update({
+      // If the address should be the default, unset all other default addresses for the user
+      if (address.default) {
+        await this.prismaService.$transaction(async (prisma) => {
+          await prisma.addresses.updateMany({
+            where: { usersId: address.userId },
+            data: { default: false },
+          });
+        });
+      }
+
+      // Update the address
+      return await this.prismaService.addresses.update({
         where: { id: address.id },
         data: {
           name: address.name,
@@ -57,79 +69,42 @@ export class UsersService {
           default: address.default,
         },
       });
-      return update ? update : null;
     }
 
-    // check have address
-    const check = await this.prismaService.addresses.findFirst({
+    // Check if the user already has an address
+    const existingAddress = await this.prismaService.addresses.findFirst({
       where: { usersId: address.userId },
     });
 
-    if (!check) {
+    if (!existingAddress) {
+      // If no existing address, set this address as default
       address.default = true;
-      return await this.prismaService.addresses.create({
-        data: {
-          name: address.name,
-          phone: address.phone,
-          address: address.address,
-          default: address.default,
-          usersId: address.userId,
-        },
+    } else if (address.default) {
+      // If there is an existing address and the new address is default, unset all other defaults
+      await this.prismaService.$transaction(async (prisma) => {
+        await prisma.addresses.updateMany({
+          where: { usersId: address.userId },
+          data: { default: false },
+        });
       });
-    } else {
-      if (address.default) {
-        return await this.prismaService.$transaction(async (prisma) => {
-          await prisma.addresses.updateMany({
-            where: { usersId: address.userId },
-            data: { default: false },
-          });
-          await prisma.addresses.create({
-            data: {
-              name: address.name,
-              phone: address.phone,
-              address: address.address,
-              default: address.default,
-              usersId: address.userId,
-            },
-          });
-          return await prisma.users.findUnique({
-            where: { id: address.userId },
-            include: { address: true },
-          });
-        });
-      } else {
-        await this.prismaService.addresses.create({
-          data: {
-            name: address.name,
-            phone: address.phone,
-            address: address.address,
-            default: address.default,
-            usersId: address.userId,
-          },
-        });
-
-        return await this.prismaService.users.findUnique({
-          where: { id: address.userId },
-          include: { address: true },
-        });
-      }
     }
 
-    // const user = await this.prismaService.users.update({
-    //   where: { id: address.userId },
-    //   data: {
-    //     address: {
-    //       create: {
-    //         name: address.name,
-    //         phone: address.phone,
-    //         address: address.address,
-    //         default: address.default,
-    //       },
-    //     },
-    //   },
-    //   include: { address: true },
-    // });
-    // return user ? user : null;
+    // Create the new address
+    await this.prismaService.addresses.create({
+      data: {
+        name: address.name,
+        phone: address.phone,
+        address: address.address,
+        default: address.default,
+        usersId: address.userId,
+      },
+    });
+
+    // Return the user with updated addresses
+    return await this.prismaService.users.findUnique({
+      where: { id: address.userId },
+      include: { address: true },
+    });
   }
 
   async updateAddress2(addressId: string) {
