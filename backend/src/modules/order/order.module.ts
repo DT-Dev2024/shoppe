7e3162a9b8ab479530529ec428fd927982e22ab0
VNKaiser <tandatvok16@gmail.com>
2024-06-30 14:44:39 +0700

    fix: payment api

--- backend/src/modules/order/order.module.ts
+++ backend/src/modules/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PaymentController } from './payment.controller';
 
 @Module({
-  controllers: [OrderController],
+  controllers: [OrderController, PaymentController],
   providers: [OrderService],
   imports: [PrismaModule],
 })
